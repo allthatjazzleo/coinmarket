91fe97bfa69837d585edad14954a4bbceb21dc33
allthatjazzleo <pangleo1994@gmail.com>
2024-03-09 01:39:52 +0800

    update flake

--- default.nix
+++ default.nix
@@ -1,11 +1,17 @@
-{rustPlatform, openssl_3_2, pkg-config}:
-rustPlatform.buildRustPackage {
-  name = "coinmarket";
-  src = ./.;
-  nativeBuildInputs = [
-    pkg-config
-  ];
-  buildInputs = [openssl_3_2];
-  cargoLock.lockFile = ./Cargo.lock;
-  RUSTFLAGS="-C link-arg=-s";
-}
+# SPDX-FileCopyrightText: 2020 Serokell <https://serokell.io/>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+(import
+  (
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  {
+    src = ./.;
+  }).defaultNix
--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -12,25 +16,54 @@
     nixpkgs,
     rust-overlay,
     flake-utils,
+    ...
   }: 
-    flake-utils.lib.eachDefaultSystem (system:
+    {
+      overlay = final: prev:
       let
-        makePkgs = config:
-          import nixpkgs {
-            inherit system;
-            overlays = [ rust-overlay.overlays.default ];
-            crossSystem = {
-              inherit config;
-              rustc = {inherit config;};
-              isStatic = true;
-            };
+        buildInputs = [] ++ final.lib.optionals final.stdenv.isDarwin (with final.darwin.apple_sdk.frameworks; [
+          SystemConfiguration
+        ]);
+        RUSTFLAGS = "-C link-arg=-s";
+      in
+      {
+        coinmarket = {}: final.rustPlatform.buildRustPackage ({
+          inherit buildInputs RUSTFLAGS;
+          pname = "coinmarket";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        }) // { meta.description = "CoinMarket TUI based on Ratatui"; };
+      };
+    } //
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          makePkgs = config:
+            let
+              shouldUseCross = builtins.elem config [
+                "aarch64-unknown-linux-musl"
+                "x86_64-unknown-linux-musl"
+              ];
+              crossSystemOptions = if shouldUseCross then {
+                crossSystem = {
+                  inherit config;
+                  rustc = {inherit config;};
+                  isStatic = true;
+                };
+              } else {};
+              pkgs = import nixpkgs (crossSystemOptions // {
+                inherit system;
+                overlays = [ rust-overlay.overlays.default self.overlay ];
+              });
+            in
+              pkgs.callPackage pkgs.coinmarket {};
+        in {
+          packages = {
+            default = makePkgs "${system}";
+            x86_64-linux-musl = makePkgs "x86_64-unknown-linux-musl";
+            aarch64-linux-musl = makePkgs "aarch64-unknown-linux-musl";
           };
-      in {
-        packages = {
-          default = (makePkgs "${system}").callPackage ./. {};
-          x86_64-linux-musl = (makePkgs "x86_64-unknown-linux-musl").callPackage ./. {};
-          aarch64-linux-musl = (makePkgs "aarch64-unknown-linux-musl").callPackage ./. {};
-        };
-      }
-    );
+        }
+      );
 }
